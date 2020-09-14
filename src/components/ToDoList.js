c22296982474b4eae2dccb149c070e4a1256e591
waiscodes <b.wais@outlook.com>
2020-09-14 14:35:10 -0600

    there's something wrong in ToDosList which threw the error of cannot get map of undefined

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const ToDoList = (props) => {
+function ToDoList(props) {
   return (
     <>
       <h2>To-Do List</h2>
       <ul>
-        {props.ToDo.map((toDo, index) => (
+        {props.toDos.map((toDo, index) => (
           <li key={index}>{toDo.task}</li>
         ))}
       </ul>
     </>
   );
-};
+}
 
+// Use connect to make our "To-Do list" available via props.toDos.
 export default connect((state) => ({ toDos: state }))(ToDoList);
