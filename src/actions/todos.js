8a3711b76f28d8c7b318c3edf471acc8ee7694bf
waiscodes <b.wais@outlook.com>
2020-09-14 11:18:59 -0600

    add a function that adds Javascript object to the to do list

---
+++ src/actions/todos.js
@@ -0,0 +1,17 @@
+/*
+ * Redux action(s)
+ * Actions as "request" formatting, or labels for what we'll allow in terms of our store.
+ * Actions define what we'll allow as requrests to our reducers (which in terns speaks directly to the store on our behalf.)
+ *Action names, by convention, are upper case (they are considered "CONSTANTS.")
+ */
+
+const addNewToDo = (toDoContent) => {
+  return {
+    //This is the NAME of the action
+    //type is is how the reducer knows what to do
+    type: "ADD_NEW_TO_DO",
+    //This is the DATA we are passing through to the reducer so that it can do ITS JOB
+    payload: toDoContent,
+  };
+  //This is just a regular JS object
+};
