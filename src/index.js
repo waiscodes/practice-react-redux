800a47134a4c618fd69d6f36ae5159dc8525609b
waiscodes <b.wais@outlook.com>
2020-09-14 11:58:03 -0600

    something isn't working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { createStore } from "redux";
+import toDosReducer from "./reducers/todos";
+import { addNewToDo } from "./actions/todos";
+
+/*
+ * Redux store
+ * Store is our global state. This is where all the global data exists.
+ * We pass in the reducer so that it'll know how to handle and actions (requests.)
+ *
+ */
+const store = createStore(toDosReducer);
+
+// We can add this to get a console log notification everytime something in the store changes.
+// subscribe is a method of createStore that allows to track changes. And you console long those changes basically.
+store.subscribe(() => console.log(store.getState()));
+
+/*
+ * Redux Dispatch
+ * This is what we use to send actions to send actions to our store's reducer.
+ */
+
+// Displatch expects a properly formatted action..
+// (otherwise your reducer won't know what to do! )
+store.dispatch(addNewToDo("Research Redux"));
 
 ReactDOM.render(
   <React.StrictMode>
